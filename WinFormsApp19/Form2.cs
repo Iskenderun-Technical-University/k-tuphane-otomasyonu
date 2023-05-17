fb8165629185470cbf7f0cfbda937f89e4284598
cmahm <cmahm@DESKTOP-TNITFUT>
2023-05-17 18:23:36 +0300

    şifre

--- WinFormsApp19/Form2.cs
+++ WinFormsApp19/Form2.cs
@@ -21,22 +21,19 @@ namespace WinFormsApp19
         private void button1_Click(object sender, EventArgs e)
         {
 
-            SqlConnection cnn = new SqlConnection("Data Source=DESKTOP-TNITFUT;Initial Catalog=kürüphane;Integrated Security=True");
-            cnn.Open();
-            SqlCommand cmd = new SqlCommand("insert into kullanici values (@kullanici,@gmail,@sifre)", cnn);
-
-            cmd.Parameters.AddWithValue("@kullanici", textBox1.Text);
-            cmd.Parameters.AddWithValue("@gmail", textBox2.Text);
-            cmd.Parameters.AddWithValue("@sifre", textBox3.Text);
-            cmd.ExecuteNonQuery();
-
-
-            MessageBox.Show("bilgi kaydedildi");
-
-            cnn.Close();
+           
             if (textBox2.Text != textBox3.Text)
             {
-                MessageBox.Show("dasdsa");
+                SqlConnection cnn = new SqlConnection("Data Source=DESKTOP-TNITFUT;Initial Catalog=kürüphane;Integrated Security=True");
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand("insert into kullanici values (@kullanici,@gmail,@sifre)", cnn);
+                cmd.Parameters.AddWithValue("@kullanici", textBox1.Text);
+                cmd.Parameters.AddWithValue("@gmail", textBox2.Text);
+                cmd.Parameters.AddWithValue("@sifre", textBox3.Text);
+
+                cmd.ExecuteNonQuery();
+                cnn.Close();
+
             }
 
 
